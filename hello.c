4cbf54771a280dba69c1f88e5446a90ed776b60b
Franklin Kong <frakong@ucdavis.edu>
2021-04-26 21:38:45 -0700

    Initial "Hello World" commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+//This is my first file. It just prints out "Hello World".
+//I wrote this file because Tic_Tac_Toe_Project is my first repository on GitHub
+//in which I intend to write a program and complete a project within it. I wanted to make 
+//sure that the Git version control features on Visual Studio Code (the editor I use)
+//were working and that the changes I made in my workspace could be pushed onto GitHub
+//successfully from VS Code.
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World!");
+}

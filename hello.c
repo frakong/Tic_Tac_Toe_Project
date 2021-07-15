bdef248ae0e8c692361c3dd03ac12dafcc27bd39
Franklin Kong <frakong@ucdavis.edu>
2021-07-14 17:46:00 -0700

    Final hello world commit, now the real fun starts!

--- hello.c
+++ hello.c
@@ -3,12 +3,15 @@
 //in which I intend to write a program and complete a project within it. I wanted to make 
 //sure that the Git version control features on Visual Studio Code (the editor I use)
 //were working and that the changes I made in my workspace could be pushed onto GitHub
-//successfully from VS Code.
+//successfully from VS Code. I made several commits where in each new commit I added
+// the line printf("Hello World!"); another time to make sure I knew how to save commits
+// on Visual Studio Code to GitHub.
 
 #include <stdio.h>
 
 int main()
 {
+  printf("Hello World!");
   printf("Hello World!");
   printf("Hello World!");
   printf("Hello World!");

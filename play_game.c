cdc2d4592f4f9c9a07b72d643012b64364af04b5
Franklin Kong <frakong@ucdavis.edu>
2021-08-05 18:02:09 -0700

    Wrote display board function

--- play_game.c
+++ play_game.c
@@ -8,11 +8,26 @@
 
 void play_game(char*** game_board, char* game_pieces, int game_board_size, int* current_turn, char blank_character){
   int moveRow, moveCol;
-  display_board();
+  display_board(*game_board, game_board_size);
   do{
     get_move_from_player();
     update_board();
-    display_board();
+    display_board(*game_board, game_board_size);
   } while (!is_game_over());
   declare_results_of_game();
+}
+
+void display_board(char** game_board, int game_board_size){
+  for (int display_row = 0; display_row < game_board_size; display_row++){
+    printf("%d  ", display_row);
+    for (int display_col = 0; display_col < game_board_size; display_col++){
+      printf("%c ", game_board[display_row][display_col]);
+    }
+    printf("\n");
+  }
+  printf("   ");
+  for (int last_line_position = 0; last_line_position < game_board_size; last_line_position++){
+    printf("%d ", last_line_position);
+  }
+  printf("\n");
 }
--- play_game.h
+++ play_game.h
@@ -2,6 +2,6 @@
 #ifndef PLAYGAME_H
   #define PLAYGAME_H
   void play_game(char*** game_board, char* game_pieces, int game_board_size, int* current_turn, char blank_character);
-  
+  void display_board(char** game_board, int game_board_size);
 
 #endif

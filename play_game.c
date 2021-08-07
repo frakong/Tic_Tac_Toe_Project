af1f63c9a32ca6c2f9fbb16a8384f88c8a473ec3
Franklin Kong <frakong@ucdavis.edu>
2021-08-06 18:50:51 -0700

    Get move from player and update board

--- play_game.c
+++ play_game.c
@@ -7,14 +7,21 @@
 #include "play_game.h"
 
 void play_game(char*** game_board, char* game_pieces, int game_board_size, int* current_turn, char blank_character){
-  int moveRow, moveCol;
-  display_board(*game_board, game_board_size);
+  int move_row, move_col;
+  displayWelcomeMessage(*game_board, game_board_size);
   do{
-    get_move_from_player();
-    update_board();
+    get_move_from_player(*game_board, game_board_size, *current_turn, &move_row, &move_col, blank_character);
+    update_board(game_board, game_pieces, move_row, move_col, *current_turn);
+    change_turn();
     display_board(*game_board, game_board_size);
   } while (!is_game_over());
   declare_results_of_game();
+}
+
+void displayWelcomeMessage(char** game_board, int game_board_size){
+  printf("Welcome! Let's begin the game!");
+  display_board(game_board, game_board_size);
+  printf("Good luck!");
 }
 
 void display_board(char** game_board, int game_board_size){
@@ -30,4 +37,38 @@ void display_board(char** game_board, int game_board_size){
     printf("%d ", last_line_position);
   }
   printf("\n");
+}
+
+void get_move_from_player(char** game_board, int game_board_size, int current_turn, int* move_row, int* move_col, char blank_character){
+  printf("Player %d, please type in your move in the following format: row# col#", current_turn+1);
+  printf("Remember, your row# and col# must be between 0 and %d", game_board_size-1);
+  do{
+    sscanf("%d %d", move_row, move_col);
+    if (in_range(*move_row, *move_col, game_board_size)){
+      if (spot_taken(game_board, *move_row, *move_col, game_board_size, blank_character)){
+        printf("That spot is already taken. Try again!");
+      }
+    }
+    else{
+      printf("That is not a valid move. Remeber, your row# and col# must be between 0 and %d", game_board_size-1);
+    }
+  } while((!in_range(*move_row, *move_col, game_board_size)) || (spot_taken(game_board, *move_row, *move_col, game_board_size, blank_character)));
+}
+
+bool spot_taken(char** game_board, int move_row, int move_col, int game_board_size, char blank_character){
+  if (game_board[move_row][move_col] == blank_character){
+    return false;
+  }
+  return true;
+}
+
+bool in_range(int move_row, int move_col, int game_board_size){
+  if ((move_row >= 0) && (move_row < game_board_size) && (move_col >= 0) &&(move_col < game_board_size)){
+    return true;
+  }
+  return false;
+}
+
+void update_board(char*** game_board, char* game_pieces, int move_row, int move_col, int current_turn){
+  *game_board[move_row][move_col] = game_pieces[current_turn];
 }
--- play_game.h
+++ play_game.h
@@ -2,6 +2,11 @@
 #ifndef PLAYGAME_H
   #define PLAYGAME_H
   void play_game(char*** game_board, char* game_pieces, int game_board_size, int* current_turn, char blank_character);
+  void displayWelcomeMessage(char** game_board, int game_board_size);
   void display_board(char** game_board, int game_board_size);
+  void get_move_from_player(char** game_board, int game_board_size, int current_turn, int* move_row, int* move_col, char blank_character);
+  bool spot_taken(char** game_board, int move_row, int move_col, int game_board_size, char blank_character);
+  bool in_range(int move_row, int move_col, int game_board_size);
+  void update_board(char*** game_board, char* game_pieces, int move_row, int move_col, int current_turn);
 
 #endif

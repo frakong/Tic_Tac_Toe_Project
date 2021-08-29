cb2a8cd2155e4e9edccb7f6601d96be5da2cecfa
Franklin Kong <frakong@ucdavis.edu>
2021-08-28 18:36:40 -0700

    Wrote comments on main.c and setup_game.c

--- main.c
+++ main.c
@@ -11,12 +11,13 @@
 #include "play_game.h"
 #include "cleanup_game.h"
 
+//This is the main function that will run the game, is also the function that the executable will run. It initializes the important variables for the game and calls the three main functions to set up the game, play the game, and clean up the game once the game is over.
 int main(int argc, char** argv){
-  char** game_board;
-  char* game_pieces;
-  int game_board_size;
-  int current_turn;
-  char blank_character = 'e';
+  char** game_board;//This is how we represent the game board, as a double character pointer. It will be an NxN grid, with N being the number of rows/columns on the board.
+  char* game_pieces;//This pointer represents an array containing the pieces the players will use to play this game: X and O.
+  int game_board_size;//This is the size of the game board that the user will input. For example, if the user inputs 3, then the size of the board will be 3x3.
+  int current_turn;//Indicates whose turn it is to place their game piece on the board. If it's 0, it's player 1's turn. If it's 1, it's player 2's turn.
+  char blank_character = 'e';//Every spot on the board that does not yet have an X or O on it is an empty space and will be filled by this character. In the future, I plan on letting the user decide what the blank character should be.
   
 
   setup_game(argc, argv, &game_board, &game_pieces, &game_board_size, &current_turn, blank_character);
--- setup_game.c
+++ setup_game.c
@@ -5,16 +5,18 @@
 #include <ctype.h>
 #include "setup_game.h"
 
+//This function allocates memory space for the game board and the array containing the game pieces (X and O), as well as establishing which player goes first.
 void setup_game(int argc, char** argv, char*** game_board, char** game_pieces, int* game_board_size, int* current_turn, char blank_character){
   *game_board = create_board(argc, argv, game_board_size, blank_character);
   *game_pieces = get_players_and_pieces();
   *current_turn = get_which_player_goes_first();
 }
 
+//This function gets the user input for the size of the board, then allocates enough memory space for this board and sets every space on the board to the blank character. Finally, it returns this newly created board.
 char** create_board(int argc, char** argv, int* game_board_size, char blank_character){
   char** board;
   fetch_game_board_size(argc, argv, game_board_size);
-  board = calloc(*game_board_size, sizeof(*board));//*board = first element in the array.
+  board = calloc(*game_board_size, sizeof(*board));
   for (int i = 0; i < *game_board_size; i++){
     board[i] = calloc(*game_board_size, sizeof(*board[i]));
     for (int j = 0; j < *game_board_size; j++){
@@ -24,16 +26,13 @@ char** create_board(int argc, char** argv, int* game_board_size, char blank_char
   return board;
 }
 
+//For now, this function will only check to make sure the user inputted one argument when he/she ran the program on the command line: The size of the board. It doesn't check whether this argument is a number or not, so in the future I plan on adding to this function to ensure more stringent error checking.
 void fetch_game_board_size(int argc, char** argv, int* game_board_size){
   if (argc == 2){
     sscanf(argv[1], "%d", game_board_size);
-    //We read in the second parameter on the command line (first is the executable),
-    // convert it to an integer, and store it as the variable game_board_size.
-    //NOTE: We do game_board_size and not &game_board_size because the parameter 
-    //game_board_size is already an integer pointer. 
   }
   else if (argc == 1){
-    printf("You must enter a number indicating the size you want the Tic Tac Toe board.\n");
+    printf("You must enter a number indicating the size you want the Tic Tac Toe board to be.\n");
     exit(EXIT_FAILURE);
   }
   else{
@@ -42,6 +41,7 @@ void fetch_game_board_size(int argc, char** argv, int* game_board_size){
   }
 }
 
+//This function simply allocates memory space for the array storing the game pieces. It will only consist of two pieces: X and O. Thus, player 1 will get X and player 2 will get O. In the future, I may implement a random generator that will randomly assign a piece to each player.
 char* get_players_and_pieces(){
   char* player_pieces = calloc(2, sizeof(*player_pieces));
   player_pieces[0] = 'X';
@@ -49,6 +49,7 @@ char* get_players_and_pieces(){
   return player_pieces;
 }
 
+//We are establishing that the player who has the X piece will go first. In the future, the selection may be random.
 int get_which_player_goes_first(){
   return 0;
 }

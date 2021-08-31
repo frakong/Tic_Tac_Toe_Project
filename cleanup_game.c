1f992522bb994b68a7da6b405b2a50701ea9bb7a
Franklin Kong <frakong@ucdavis.edu>
2021-08-30 18:24:31 -0700

    Added some comments to play_game.c

--- cleanup_game.c
+++ cleanup_game.c
@@ -6,17 +6,20 @@
 #include <ctype.h>
 #include "cleanup_game.h"
 
+//This function frees up the memory space allocated to the arrays for the game pieces and the game board.
 void cleanup_game(char** game_board, char* game_pieces, int game_board_size){
   cleanup_board(game_board, game_board_size);
   cleanup_game_pieces(game_pieces);
 }
 
+//Frees up the memory space allocated for the NxN game board.
 void cleanup_board(char** game_board, int game_board_size){
   for (int row = 0; row < game_board_size; row++){
     free(game_board[row]);
   }
 }
 
+//Frees up the memory space allocated for the array that lists the game pieces.
 void cleanup_game_pieces(char* game_pieces){
   free(game_pieces);
 }
--- play_game.c
+++ play_game.c
@@ -6,73 +6,86 @@
 #include <ctype.h>
 #include "play_game.h"
 
+//Main function for playing the game. Starts out by displaying a welcome message, then continues to read in moves inputted by the player, update the board, display the board, and change turns until either someone has won or there is a tie. Finally, it declares the results of the game. 
 void play_game(char*** game_board, char* game_pieces, int game_board_size, int* current_turn, char blank_character){
   int move_row, move_col;
   displayWelcomeMessage(*game_board, game_board_size);
   do{
-    change_turn(current_turn);
     get_move_from_player(*game_board, game_board_size, *current_turn, &move_row, &move_col, blank_character);
     update_board(game_board, game_pieces, move_row, move_col, *current_turn);
     display_board(*game_board, game_board_size);
+    if (!is_game_over(*game_board, game_board_size, blank_character)){
+      change_turn(current_turn);
+    }
   } while (!is_game_over(*game_board, game_board_size, blank_character));
+  //When we exit the do-while loop, if a player has won, the current turn will be that player.
   declare_results_of_game(*game_board, game_board_size, blank_character, *current_turn);
 }
 
+//Displays the initialized board and a welcome message to signify the beginning of the game.
 void displayWelcomeMessage(char** game_board, int game_board_size){
   printf("Welcome! Let's begin the game!");
   display_board(game_board, game_board_size);
   printf("Good luck!");
 }
 
+//Prints the current state of the board in grid format to the screen.
 void display_board(char** game_board, int game_board_size){
   for (int display_row = 0; display_row < game_board_size; display_row++){
-    printf("%d  ", display_row);
+    printf("%d  ", display_row);//Write out the row numbers.
     for (int display_col = 0; display_col < game_board_size; display_col++){
-      printf("%c ", game_board[display_row][display_col]);
+      printf("%c ", game_board[display_row][display_col]);//First column of board will be printed 2 spaces after the row number to create separation.
     }
     printf("\n");
   }
-  printf("   ");
+  printf("   ");//First column of board will be printed after 3 spaces.
   for (int last_line_position = 0; last_line_position < game_board_size; last_line_position++){
-    printf("%d ", last_line_position);
+    printf("%d ", last_line_position);//write out the column numbers.
   }
   printf("\n");
 }
 
+//Reads in input from player (in format: row# col#) and checks if this row/column is valid.
 void get_move_from_player(char** game_board, int game_board_size, int current_turn, int* move_row, int* move_col, char blank_character){
   printf("Player %d, please type in your move in the following format: row# col#", current_turn+1);
   printf("Remember, your row# and col# must be between 0 and %d", game_board_size-1);
   do{
-    sscanf("%d %d", move_row, move_col);
+    sscanf("%d %d", move_row, move_col);//No dereference because move_row and move_col were passed as pointers.
     if (in_range(*move_row, *move_col, game_board_size)){
       if (spot_taken(game_board, *move_row, *move_col, game_board_size, blank_character)){
         printf("That spot is already taken. Try again!");
       }
     }
     else{
-      printf("That is not a valid move. Remeber, your row# and col# must be between 0 and %d", game_board_size-1);
+      printf("That is not a valid move. Remeber, your row# and col# must be between 0 and %d. Try again!", game_board_size-1);
     }
-  } while((!in_range(*move_row, *move_col, game_board_size)) || (spot_taken(game_board, *move_row, *move_col, game_board_size, blank_character)));
+  } while((!in_range(*move_row, *move_col, game_board_size)) || (spot_taken(game_board, *move_row, *move_col, game_board_size, blank_character)));//Move must correspond to a currently empty spot on the board and be within the board (in range) to be valid.
 }
 
+//Checks if a certain row&column number on the game board already has an X or O on it.
 bool spot_taken(char** game_board, int move_row, int move_col, int game_board_size, char blank_character){
   if (game_board[move_row][move_col] == blank_character){
+    //Spot is not taken if a blank character occupies it.
     return false;
   }
   return true;
 }
 
+//Checks if a certain row and column number is within the dimensions of the board.
 bool in_range(int move_row, int move_col, int game_board_size){
   if ((move_row >= 0) && (move_row < game_board_size) && (move_col >= 0) &&(move_col < game_board_size)){
+    //row and column number must be between 0 and the size of the board.
     return true;
   }
   return false;
 }
 
+//Changes the entry at row move_row, column move_col on the board to the current player's piece.
 void update_board(char*** game_board, char* game_pieces, int move_row, int move_col, int current_turn){
   *game_board[move_row][move_col] = game_pieces[current_turn];
 }
 
+//Changes the player whose turn it is to make a move. Right now, I assume there are only two players. In the future, I might modify this function so there can be more players.
 void change_turn(int* currentTurn){
   if (*currentTurn == 0){
     *currentTurn = 1;
@@ -82,6 +95,7 @@ void change_turn(int* currentTurn){
   }
 }
 
+//Checks if there is either a tie or a player has won the game.
 bool is_game_over(char** game_board, int game_board_size, char blank_character){
   if (someone_won(game_board, game_board_size, blank_character) || is_tie(game_board, game_board_size, blank_character)){
     return true;
